90b8b5d1736e8ca6f07724663bbb3958f07a1754
Shaun McThomas <smcthoma@uci.edu>
2016-03-30 22:52:11 -0700

    starting repository

---
+++ Project 1/createtable.sql
@@ -0,0 +1,81 @@
+DROP DATABASE IF EXISTS moviedb;
+CREATE DATABASE IF NOT EXISTS moviedb;
+
+use moviedb;
+	
+CREATE TABLE IF NOT EXISTS movies(
+	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL, 
+    title VARCHAR(100) NOT NULL,
+	year INT NOT NULL,
+	director VARCHAR(100) NOT NULL,
+	banner_url VARCHAR(200),
+	trailer VARCHAR(200)
+	);
+
+CREATE TABLE IF NOT EXISTS stars(
+	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL, 
+    frist_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(50) NOT NULL,
+	dob DATE,
+	photo_url VARCHAR(200)
+	);
+
+CREATE TABLE IF NOT EXISTS stars_in_movies(
+	star_id INT NOT NULL,
+	movies_id INT NOT NULL,
+	FOREIGN KEY (star_id) REFERENCES stars(id)
+        ON DELETE CASCADE,	
+	FOREIGN KEY (movies_id) REFERENCES movies(id)
+        ON DELETE CASCADE			
+	);
+	
+CREATE TABLE IF NOT EXISTS genres(
+	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	name VARCHAR(32) NOT NULL
+	);
+
+CREATE TABLE IF NOT EXISTS genres_in_movies(
+	genres_id INT NOT NULL,
+	movies_id INT NOT NULL,
+	FOREIGN KEY (genres_id) REFERENCES genres(id)
+		ON DELETE CASCADE,
+	FOREIGN KEY (movies_id) REFERENCES movies(id)
+		ON DELETE CASCADE
+	);
+    
+CREATE TABLE IF NOT EXISTS creditcards(
+	id VARCHAR(20) PRIMARY KEY NOT NULL,
+	frist_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(50) NOT NULL,
+	expiration DATE NOT NULL
+	);
+    
+CREATE TABLE IF NOT EXISTS customers(
+	id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	frist_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(50) NOT NULL,
+	cc_id VARCHAR(20) NOT NULL,
+	address VARCHAR(200) NOT NULL,
+	email VARCHAR(50) NOT NULL,
+	password VARCHAR(20) NOT NULL,
+	FOREIGN KEY (cc_id) REFERENCES creditcards(id)
+	);
+
+CREATE TABLE IF NOT EXISTS sales(
+	id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	customer_id INT NOT NULL,
+	movies_id INT NOT NULL,
+	sale_date DATE NOT NULL,
+	FOREIGN KEY (customer_id) REFERENCES customers(id),
+	FOREIGN KEY (movies_id) REFERENCES movies(id)
+	);
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
