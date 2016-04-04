c2b902192461eae738e496378aaa086d3ccfef50
HenryWoodstock <HenryWoodstock>
2016-04-03 17:40:37 -0700

    Completed insert_new_star
    finished the task where we need to be able to insert a star.  Also
    changed the column name "frist_name" in the stars table.  That is a typo

---
+++ Project 1/dbFunctions.java
@@ -0,0 +1,51 @@
+ 
+import java.sql.*;
+
+public class dbFunctions
+{
+	private Connection connection;
+	public void main(String[] args) throws Exception
+	{
+
+	}
+
+	/**
+		Establishes connection to database
+		@param path - path to db
+		@param user_name - mysql username
+		@param pass - mysql password
+	*/
+	public void make_connection(String path, String user_name, String pass) throws Exception
+	{
+		Class.forName("com.mysql.jdbc.Driver").newInstance();
+		connection = DriverManager.getConnection(path, user_name, pass);
+	}
+
+	public String append_string_array(String str, String[] to_append)
+	{
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < to_append.length; ++i)
+		{
+   			sb.append(to_append[i]);
+    		if (i != to_append.length - 1)
+       			sb.append(",");
+       	}
+       	return str.concat(sb.toString());
+    
+	}
+	public ResultSet select(String stmt) throws Exception
+	{
+		Statement select = connection.createStatement();
+		ResultSet results = select.executeQuery(stmt);
+		return results;
+	}
+
+	public int update(String stmt) throws Exception
+	{
+		Statement update = connection.createStatement();
+		int results = update.executeUpdate(stmt);
+		return results;
+
+	}
+
+}
