6b329cdf75f4339e29bb2d50cb7fc9f3413ff249
Shaun McThomas <smcthoma@uci.edu>
2016-04-04 16:38:48 -0700

    a db fgunction dont

--- Project 1/dbFunctions.java
+++ Project 1/dbFunctions.java
@@ -1,70 +1,8 @@
-<<<<<<< HEAD
  
 import java.sql.*;
-
-public class dbFunctions
-{
-	private Connection connection;
-	public void main(String[] args) throws Exception
-	{
-
-	}
-
-	/**
-		Establishes connection to database
-		@param path - path to db
-		@param user_name - mysql username
-		@param pass - mysql password
-	*/
-	public void make_connection(String path, String user_name, String pass) throws Exception
-	{
-		Class.forName("com.mysql.jdbc.Driver").newInstance();
-		connection = DriverManager.getConnection(path, user_name, pass);
-	}
-
-	public String append_string_array(String str, String[] to_append)
-	{
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < to_append.length; ++i)
-		{
-   			sb.append(to_append[i]);
-    		if (i != to_append.length - 1)
-       			sb.append(",");
-       	}
-       	return str.concat(sb.toString());
-    
-	}
-	public ResultSet select(String stmt) throws Exception
-	{
-		Statement select = connection.createStatement();
-		ResultSet results = select.executeQuery(stmt);
-		return results;
-	}
-
-	public int update(String stmt) throws Exception
-	{
-		Statement update = connection.createStatement();
-		int results = update.executeUpdate(stmt);
-		return results;
-
-	}
-	public int update(String stmt, String[] args) throws Exception
-	{
-		PreparedStatement update = connection.prepareStatement(stmt);
-		for(int i = 1; i <= args.length; ++i)
-		{
-			update.setObject(i, args[i-1]);
-		}
-		int results = update.executeUpdate();
-		return results;
-
-	}
-
-}
-=======
- 
-import java.sql.*;
-import java.utils.ArrayList;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 
 public class dbFunctions
 {
@@ -110,33 +48,45 @@ public class dbFunctions
 
 	}
 
-	public ArrayList< HashMap< String, Object > > selectUsing(String table, String columns, String values)
+	public ArrayList< HashMap< String, Object > > selectUsing(String table, String columns, String values) throws SQLException
 	{
 
-		String statement = "SELECT * FROM " + table ;
-		ArrayList<String> listOfColums = Arrays.asList(columns.split(","));
-		ArrayList<String> listOfvalues = Arrays.asList(values.split(","));
+		String statementString = "SELECT * FROM " + table ;
+		ArrayList<String> listOfColums = (ArrayList<String>) Arrays.asList(columns.split(","));
+		ArrayList<String> listOfvalues = (ArrayList<String>) Arrays.asList(values.split(","));
 		if(listOfColums.size() > 0)
 		{
-			statement += " WHERE " + listOfColums.at(i) + "=" + listOfvalues.at(i);	
+			statementString += " WHERE " + listOfColums.get(0) + "=" + listOfvalues.get(0);	
 			for(int i = 0; i < listOfColums.size(); i++)
 			{
 
-				statement +=  " AND " + listOfColums.at(i) + "=" + listOfvalues.at(i);
+				statementString +=  " AND " + listOfColums.get(i) + "=" + listOfvalues.get(i);
 			}
 		}
-		statement += ";";
+		statementString += ";";
+		PreparedStatement statement = connection.prepareStatement(statementString);
+		ResultSet results = statement.executeQuery();
+		ArrayList< HashMap< String, Object > > output = new ArrayList< HashMap< String, Object > >();
+		ResultSetMetaData rsmd = results.getMetaData();
+		int numCol = rsmd.getColumnCount();
+		ArrayList<String> coulumNames = new  ArrayList<String>();
+		for(int i =1; i <= numCol; i++)
+		{
+			coulumNames.add(rsmd.getColumnLabel(i));
+		}
+		while(results.next())
+		{
+			 
+			HashMap< String, Object >temp =new  HashMap< String, Object >();
+			for(String currCol : coulumNames)
+			{
+				temp.put(currCol, results.getObject(currCol));
+			}
+			output.add(temp);
+			
+		}
+		return output;
 	}
 }
 
 
-
-
-
-
-
-
-
-
-
->>>>>>> c73b359db4ab7c7b52bdfb518d379a38161635a3
