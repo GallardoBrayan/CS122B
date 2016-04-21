06723f62ba12c80c0333505feeaf18ff7dac43ba
Shaun McThomas <smcthoma@uci.edu>
2016-04-20 21:04:43 -0700

    Let's sync up!!!!!!!!

--- Project 2/fabflix/WebContent/WEB-INF/source/fabflix/LoginHandler.java
+++ Project 2/fabflix/WebContent/WEB-INF/source/fabflix/LoginHandler.java
@@ -2,7 +2,6 @@ package fabflix;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,7 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class LoginHandler
  */
-@WebServlet(description = "New Method to handle logging in", urlPatterns = { "/LoginHandler" })
+
 public class LoginHandler extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
