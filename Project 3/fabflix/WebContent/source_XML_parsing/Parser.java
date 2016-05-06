08573e9c5db362242649290cc2f9215608b16295
HenryWoodstock <HenryWoodstock>
2016-05-05 23:31:09 -0700

    ta stuff in there

--- Project 3/fabflix/WebContent/source_XML_parsing/Parser.java
+++ Project 3/fabflix/WebContent/source_XML_parsing/Parser.java
@@ -44,7 +44,7 @@ public class Parser extends DefaultHandler{
 			//get a new instance of parser
 			SAXParser sp = spf.newSAXParser();
 			try{
-				conn.make_connection("jdbc:mysql://localhost:3306/moviedb", "root", "root");
+				conn.make_connection("jdbc:mysql://localhost:3306/moviedb_project3_grading", "classta", "classta");
 				}catch(Exception e)
 				{}
 			//parse the file and also register this class for call backs
